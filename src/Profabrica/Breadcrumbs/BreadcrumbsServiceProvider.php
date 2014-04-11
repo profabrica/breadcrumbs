1b6de5af18ee4e955106f097b85488131294a164
Lukasz Szymkowiak <lukasz@profabrica.pl>
2014-04-11 12:12:33 +0200

    init

---
+++ src/Profabrica/Breadcrumbs/BreadcrumbsServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Profabrica\Breadcrumbs;
+
+use Illuminate\Support\ServiceProvider;
+
+class BreadcrumbsServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('creitive/breadcrumbs');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['breadcrumbs'] = $this->app->share(function($app)
+		{
+			return new Breadcrumbs;
+		});
+	}
+
+}
---
+++ src/Profabrica/Breadcrumbs/Facades/Breadcrumbs.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Profabrica\Breadcrumbs\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Breadcrumbs extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'breadcrumbs'; }
+
+}
